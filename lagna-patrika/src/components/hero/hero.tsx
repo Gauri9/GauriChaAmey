e1df71d77160f7c431a35834bba8444fa7b099ef
Gauri9 <gauriwalke9@gmail.com>
2026-08-19 22:50:55 +0530

    Configure GitHub Pages

--- lagna-patrika/src/components/hero/hero.tsx
+++ lagna-patrika/src/components/hero/hero.tsx
@@ -12,21 +12,6 @@ const container = {
   },
 };
 
-const item = {
-  hidden: {
-    opacity: 0,
-    y: 25,
-  },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.8,
-      ease: "easeOut",
-    },
-  },
-};
-
 export default function Hero() {
 
   const { language } = useLanguage();
